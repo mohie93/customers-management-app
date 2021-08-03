f8abc88e5f65a094e2823156c1c855f33b0bc26d
mohie93 <mohie_93@hotmail.com>
2021-08-04 00:05:05 +0800

    Added filter and sort

--- customer-app-ui/src/app/components/customers-list/customers-list.component.ts
+++ customer-app-ui/src/app/components/customers-list/customers-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { CustomersService } from '../../services/customers.service';
 import { Customer } from '../../models/Customer';
 
@@ -12,6 +12,14 @@ export class CustomersListComponent implements OnInit {
 
   customers?: Customer[];
   hasCustomers?: boolean = false;
+  firstNameDirection: string = 'asc';
+  lastNameDirection: string = 'asc';
+  createdAtDirection: string = 'asc';
+  searchNeedle: string = '';
+
+  @HostListener('input') oninput() {
+    this.searchCustomers();
+  }
 
   ngOnInit(): void {
     this.retrieveCustomers();
@@ -38,5 +46,48 @@ export class CustomersListComponent implements OnInit {
         console.log(error);
       }
     );
+  }
+
+  searchCustomers(): void {
+    if (this.searchNeedle === '') return this.retrieveCustomers();
+    else {
+      this.customerService.searchBy(this.searchNeedle).subscribe(
+        (data) => {
+          this.customers = data;
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
+  }
+
+  sort(attribute: string, direction: string): void {
+    this.customerService.OrderBy(attribute, direction).subscribe(
+      (data) => {
+        this.customers = data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  sortByFirstName(): void {
+    if (this.firstNameDirection === 'asc') this.firstNameDirection = 'desc';
+    else this.firstNameDirection = 'asc';
+    return this.sort('firstName', this.firstNameDirection);
+  }
+
+  sortByLastName(): void {
+    if (this.lastNameDirection === 'asc') this.lastNameDirection = 'desc';
+    else this.lastNameDirection = 'asc';
+    return this.sort('lastName', this.lastNameDirection);
+  }
+
+  sortByCreatedAt(): void {
+    if (this.createdAtDirection === 'asc') this.createdAtDirection = 'desc';
+    else this.createdAtDirection = 'asc';
+    return this.sort('createdAt', this.createdAtDirection);
   }
 }
